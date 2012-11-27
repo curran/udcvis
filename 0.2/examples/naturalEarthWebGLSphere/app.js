22ab968ec00302634c47cf18ab8c27f542f4f6ea
curran <curran.kelleher@gmail.com>
2012-11-27 18:35:46 -0500

    Resized image to power of 2

---
+++ 0.2/examples/naturalEarthWebGLSphere/app.js
@@ -0,0 +1,10 @@
+// An app that uses the OpenGL ES API
+// to display the Natural Earth top-level satellite images
+// as a texture
+// on a sphere
+// with mouse rotation
+// orthographic projection
+// possibly display Quadstream output
+// with same projection
+// using Canvas.
+// for choropleth maps
