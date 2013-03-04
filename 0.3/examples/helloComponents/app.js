1a1ed11894dea19ab7ed06b7d75c364c2492d7af
curran <curran.kelleher@gmail.com>
2013-03-04 11:48:08 -0500

    Added LinearComponent pseudoclass for HBox and VBox

--- 0.3/examples/helloComponents/app.js
+++ 0.3/examples/helloComponents/app.js
@@ -1,15 +1,36 @@
 // An outline of a Component concept
 require(['udc/FullScreenCanvas', 
          'app/ColoredXComponent', 
+         'app/LinearComponent', 
          'app/SquareComponent'], 
     function(FullScreenCanvas, 
              ColoredXComponent,
+             LinearComponent,
              SquareComponent){
 
 //  var rootComponent = new XComponent();
-  var rootComponent = new SquareComponent(
-    new ColoredXComponent('red')
+  //var rootComponent = new SquareComponent(
+  //  new ColoredXComponent('red')
+  //);
+  var rootComponent = new LinearComponent('vertical'),
+      top = new LinearComponent('horizontal'),
+      middle = new LinearComponent('vertical');
+
+  top.addChild( new ColoredXComponent('red'), {size: 1});
+  top.addChild(middle, {size: 1});
+  top.addChild( new ColoredXComponent('green'), {size: 1});
+
+  middle.addChild( new ColoredXComponent('red'), {size: 1});
+  middle.addChild( new ColoredXComponent('green'), {size: 1});
+  middle.addChild( new ColoredXComponent('blue'), {size: 1});
+
+  rootComponent.addChild(top, {size:1});
+  rootComponent.addChild( 
+    new SquareComponent(
+      new ColoredXComponent('blue')
+    ), {size: 1}
   );
+
 
   // `render` is called 60 times per second.
   function render(c, box, resized){
---
+++ 0.3/examples/helloComponents/app/LinearComponent.js
@@ -0,0 +1,65 @@
+define(['app/Component','udc/Rectangle'], 
+    function(Component, Rectangle){
+
+  var superConstructor = Component,
+      proto = new superConstructor(),
+      parent = superConstructor.prototype,
+      tempRect = new Rectangle();
+  function LinearComponent(orientation){
+    superConstructor.call(this);
+    this.children = [];
+    this.orientation = orientation;
+  }
+  LinearComponent.prototype = proto;
+
+  // options: size:Number
+  proto.addChild = function(component, options){
+    this.children.push({
+      component: component,
+      options: options
+    });
+    //component.on('dirty', function(){
+    //  this.dirty = true;
+    //  this.trigger('dirty');
+    //}
+  }
+  
+  proto.draw = function(c, box){
+    var i, n = this.children.length, child,
+        childSize, totalSize = 0, sizeSoFar = 0, 
+        p1, p2;
+    for(i = 0; i < n; i++){
+      child = this.children[i];
+      childSize = child.options.size;
+      totalSize += childSize;
+    }
+    for(i = 0; i < n; i++){
+      child = this.children[i];
+      childSize = child.options.size;
+      p1 = sizeSoFar / totalSize;
+      sizeSoFar += childSize;
+      p2 = sizeSoFar / totalSize;
+
+      if(this.orientation == 'horizontal'){
+        tempRect.x1 = box.x + box.w * p1;
+        tempRect.x2 = box.x + box.w * p2;
+        tempRect.y = box.y;
+        tempRect.h = box.h;
+      }
+      else if(this.orientation == 'vertical'){
+        tempRect.y1 = box.y + box.h * p1;
+        tempRect.y2 = box.y + box.h * p2;
+        tempRect.x = box.x;
+        tempRect.w = box.w;
+      }
+
+      child.component.draw(c, new Rectangle(
+        tempRect.x, tempRect.y, tempRect.w, tempRect.h
+      ));
+//      child.component.draw(c, tempRect);
+    }
+    this.isDirty = false;
+  };
+
+  return LinearComponent;
+});
